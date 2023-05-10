4a76dbb274d1d052fd202e254abcfdf76cc455ea
root <mgiraudo@ies21.edu.ar>
2023-05-10 13:52:15 -0300

    Agregar archivos de proyecto.

---
+++ pryBasedeDatos2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace pryBasedeDatos2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.dtvDatos = new System.Windows.Forms.DataGridView();
+            this.CTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 23);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // dtvDatos
+            // 
+            this.dtvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CTipo,
+            this.CNombre,
+            this.CId,
+            this.CPrecio});
+            this.dtvDatos.Location = new System.Drawing.Point(12, 81);
+            this.dtvDatos.Name = "dtvDatos";
+            this.dtvDatos.Size = new System.Drawing.Size(500, 150);
+            this.dtvDatos.TabIndex = 1;
+            // 
+            // CTipo
+            // 
+            this.CTipo.HeaderText = "Tipo De Producto";
+            this.CTipo.Name = "CTipo";
+            // 
+            // CNombre
+            // 
+            this.CNombre.HeaderText = "Nombre De Producto";
+            this.CNombre.Name = "CNombre";
+            // 
+            // CId
+            // 
+            this.CId.HeaderText = "Id Grupo";
+            this.CId.Name = "CId";
+            // 
+            // CPrecio
+            // 
+            this.CPrecio.HeaderText = "Precio";
+            this.CPrecio.Name = "CPrecio";
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(12, 52);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 2;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(148, 250);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(12, 250);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(55, 250);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(87, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 287);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.dtvDatos);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Coneccion a base de datos";
+            ((System.ComponentModel.ISupportInitialize)(this.dtvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.DataGridView dtvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPrecio;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecio;
+    }
+}
+
---
+++ pryBasedeDatos2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryBasedeDatos2
+{
+    public partial class frmMain : Form
+    {
+
+        OleDbConnection ConnBase = null;
+        OleDbCommand CmdBase = null;
+        OleDbDataReader LectorBD = null;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            ConnBase = new OleDbConnection();
+            ConnBase.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\VERDULEROS.mdb";
+            ConnBase.Open();
+
+            MessageBox.Show("Conectado a BD");
+            btnConectar.BackColor = Color.Green;
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            CmdBase = new OleDbCommand();
+            CmdBase.Connection = ConnBase;
+            CmdBase.CommandType = CommandType.TableDirect;
+            CmdBase.CommandText = "Productos";
+        
+            LectorBD = CmdBase.ExecuteReader();
+
+            while (LectorBD.Read())
+            {
+                dtvDatos.Rows.Add(LectorBD[0],LectorBD[1], LectorBD[2], LectorBD[3]);
+               
+            }
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
